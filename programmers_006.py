cbf92d449898243ca08d2e37c34cce698fc0d30e
hyunchan123 <tmzkdlqmfn1@naver.com>
2021-07-05 09:16:31 +0900

    소수찾기

---
+++ programmers_006.py
@@ -0,0 +1,8 @@
+# 소수찾기
+def solution(n):
+    a = [False, False] + [True]*(n-1)
+    for i in range(2, n+1):
+        if a[i]:
+            for j in range(i*2, n+1, i):
+                a[j] = False
+    return a.count(True)
